40500663139e5eb0c24a410d90404c19b17766e9
Bdani2201 <91455909+Bdani2201@users.noreply.github.com>
2022-03-22 09:15:31 +0100

    5-6 feladat

--- balkezesek/Program.cs
+++ balkezesek/Program.cs
@@ -49,7 +49,31 @@ namespace balkezesek
             for (i = 0; i < adatokszama; i++)
                 if (adatok[i].Utolso.Year == 1999 && adatok[i].Utolso.Month == 10)
                     Console.WriteLine("\t{0}, {1} cm", adatok[i].Nev, Math.Round(adatok[i].Magassag * 2.54, 1));
+            //5
+            Console.WriteLine("5.feladat:");
+            Console.Write("Kérek egy 1990 és 1999 közötti évszámot!: ");
+            int evszam = Convert.ToInt32(Console.ReadLine());
+            while (!(evszam >= 1990 && evszam <= 1999))
+            {
+                Console.Write("Hibás adat, kérek egy 1990 és 1999 közötti évszámot!: ");
+               
+                evszam = Convert.ToInt32(Console.ReadLine());
+            }
+            //6
+            double atlagsuly = 0;
+            int db = 0;
+            for (i = 0; i < adatokszama; i++)
+                if (evszam >= adatok[i].Elso.Year && evszam <= adatok[i].Utolso.Year)
+                {
+                    atlagsuly += adatok[i].Suly;
+                    db++;
+                }
+            Console.WriteLine("6. feladat: {0} font", Math.Round(atlagsuly / db, 2));
 
+            Console.ReadKey();
+        }
+    }
+}
             
 
             
